d0e2b0b307c0df974493e6a0399169e02d5aacc1
Mateusz Miłkowski <mat.milkowski@gmail.com>
2023-12-14 12:24:05 +0100

    Add display of tasks on the front end

--- TodoClient/src/app/app.component.ts
+++ TodoClient/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TodoClient';
+  userTasks : any;
+  apiUrl = 'http://localhost:5000/api/tasks';
+
+  constructor(private http: HttpClient) 
+  {
+    
+  }
+  ngOnInit(): void {
+    this.http.get(this.apiUrl).subscribe({
+      next: response => this.userTasks = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    })
+  }
+
+
 }
